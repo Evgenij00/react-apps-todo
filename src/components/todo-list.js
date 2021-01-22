cbf1cd416d84151b8ce2cb5bd99a5de6959ba179
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2021-01-22 14:13:46 +0300

    добавил ключи для списка

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -5,8 +5,11 @@ const TodoList = ({ todos }) => {
 
     const elements = todos.map( (item) => {
 
+        const {id, ...itemProps} = item
+
         return (
-            <li><TodoListItem {...item} />
+            <li key={id}>
+                <TodoListItem {...itemProps} />
             </li>
         );
     });
