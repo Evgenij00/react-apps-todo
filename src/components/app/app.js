151e40e8708389791ec47921ebd641ff6bd0045a
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2021-01-23 15:07:32 +0300

    Добавил state для элементов списка

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,26 +7,45 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-      { label: "Drink Coffee", important: false, id: 1 },
-      { label: "Make Awesome App", important: true, id: 2 },
-      { label: "Have a lunch", important: false, id: 3 },
-    ];
-  
-    return (
-      <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
-  
-        <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
-        </div>
-  
-        <TodoList todos={todoData} />
-      </div>
-    )
-  }
+    state = {
+        todoData: [
+            { label: "Drink Coffee", important: false, id: 1 },
+            { label: "Make Awesome App", important: true, id: 2 },
+            { label: "Have a lunch", important: false, id: 3 },
+        ],
+    }
 
-  export default App
+    deleteItem = (id) => {
+        this.setState( ({ todoData }) => {
+            let idx = todoData.findIndex( (el) => el.id === id )
+
+            let newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1),
+            ]
+
+            return {
+                todoData: newArray,
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="todo-app">
+              <AppHeader toDo={1} done={3} />
+        
+              <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+              </div>
+        
+              <TodoList todos={this.state.todoData} 
+                  onDeleted={ this.deleteItem }
+              />
+            </div>
+        )
+    }
+}
